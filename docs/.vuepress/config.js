c95fdf73b85c07c71573339c44797961e615fa28
jiannei <longjian.huang@aliyun.com>
2020-03-15 16:36:24 +0800

    chore: 升级依赖

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
-const {readFileSync} = require('fs')
+const {readFileSync} = require('fs');
 const { resolve } = require('path');
 
-function config(key,value){
+function config(key){
     const cfgPath = resolve(`./config/${key}.json`)
     const cfg = readFileSync(cfgPath).toString()
     return JSON.parse(cfg)
